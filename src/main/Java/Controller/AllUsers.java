82126dacb39a44757b179da1b58b243d66c71783
Paul Norby <paulnorby@me.com>
2018-10-03 13:22:24 -0500

    I updated the getAllUsers method

---
+++ src/main/Java/Controller/AllUsers.java
@@ -0,0 +1,32 @@
+package Controller;
+
+import Persistence.UserDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * A simple servlet to welcome the user.
+ * @author pwaite
+ */
+
+@WebServlet(
+        urlPatterns = {"/allUsers"}
+)
+
+public class AllUsers extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        boolean initialLoad = true;
+        UserDao userDao = new UserDao();
+        req.setAttribute("users", userDao.getAllUsers());
+        req.setAttribute("initial", initialLoad);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/results.jsp");
+        dispatcher.forward(req, resp);
+    }
+}
