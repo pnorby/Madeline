e3af2eb6de1c3338620d7cf7b62497f4b12ab225
Paul Norby <paulnorby@me.com>
2018-10-09 20:40:26 -0500

    I added the location entity.

---
+++ src/main/Java/Entity/Location.java
@@ -0,0 +1,145 @@
+package Entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+/**
+ * A class to represent a location.
+ *
+ * @author pnorby
+ */
+@Entity(name = "Location")
+@Table(name = "location")
+public class Location {
+    @Column(name = "location_name")
+    private String locationName;
+    @Column(name = "location_address")
+    private String locationAddress;
+    @Column(name = "location_city")
+    private String locationCity;
+    @Column(name = "location_state")
+    private String locationState;
+    @Column(name = "location_zip")
+    private String locationZip;
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO, generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
+    private int id;
+
+
+    /**
+     * Instantiates a new Location.
+     */
+    public Location() {
+    }
+
+    /**
+     * Instantiates a new Location.
+     *
+     * @param locationName    the location name
+     * @param locationAddress the address of the location
+     * @param locationCity    the location city
+     * @param locationState   the location state
+     * @param locationZip     the location zip
+     * @param id              the location id
+     */
+    public Location(String locationName, String locationAddress, String locationCity, String locationState, String locationZip, int id) {
+        this.locationName = locationName;
+        this.locationAddress = locationAddress;
+        this.locationCity = locationCity;
+        this.locationState = locationState;
+        this.locationZip = locationZip
+        this.id = id;
+    }
+
+    /**
+     * Gets location name.
+     *
+     * @return the location name
+     */
+    public String getLocationName() {
+        return locationName;
+    }
+
+    /**
+     * Sets location name.
+     *
+     * @param locationName the location name
+     */
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+    /**
+     * Gets location address.
+     *
+     * @return the location address
+     */
+    public String getLocationAddress() {
+        return locationAddress;
+    }
+
+    /**
+     * Sets location address.
+     *
+     * @param locationAddress the location address
+     */
+    public void setLocationAddress(String locationAddress) {
+        this.locationAddress = locationAddress;
+    }
+
+    /**
+     * Gets location city.
+     *
+     * @return the location city
+     */
+    public String getLocationCity() {
+        return locationCity;
+    }
+
+    /**
+     * Sets location city.
+     *
+     * @param locationCity the location city
+     */
+    public void setLocationCity(String locationCity) {
+        this.locationCity = locationCity;
+    }
+
+    /**
+     * Gets location state.
+     *
+     * @return the location state
+     */
+    public String getLocationState() {
+        return locationState;
+    }
+
+    /**
+     * Sets location state.
+     *
+     * @param locationState the location state
+     */
+    public void setLocationState(String locationState) {
+        this.locationState = locationState;
+    }
+
+    /**
+     * Gets location zip.
+     *
+     * @return the location zip
+     */
+    public String getLocationZip() {
+        return locationZip;
+    }
+
+    /**
+     * Sets location zip.
+     *
+     * @param locationZip the location zip
+     */
+    public void setLocationZip(String locationZip) {
+        this.locationZip = locationZip;
+    }
+}
--- src/main/Java/Entity/Trip.java
+++ src/main/Java/Entity/Trip.java
@@ -6,51 +6,125 @@ import javax.persistence.*;
 import java.time.*;
 
 
-    /**
-     * A class to represent a trip.
-     *
-     * @author pnorby
-     */
-
-    @Entity(name = "Trip")
+/**
+ * A class to represent a trip.
+ *
+ * @author pnorby
+ */
+@Entity(name = "Trip")
     @Table(name = "trip")
     public class Trip {
+
+
         @Column(name = "trip_location_id")
         private int locationId;
+
         @Column(name = "trip_name")
         private String tripName;
+
         @Column(name = "trip_start_date")
         private LocalDate tripStartDate;
+
         @Column(name = "trip_end_date")
         private LocalDate tripEndDate;
+
         @Id
         @GeneratedValue(strategy= GenerationType.AUTO, generator="native")
         @GenericGenerator(name = "native",strategy = "native")
         private int id;
 
-
-
-        /**
-         * Instantiates a new User.
-         */
-        public Trip() {
+    /**
+     * Instantiates a new User.
+     */
+    public Trip() {
         }
 
-        /**
-         * Instantiates a new User.
-         *
-         * @param tripName the trip name
-         * @param locationId the id of the location of the trip
-         * @param tripStartDate the date the trip begins
-         * @param tripEndDate the date the trip ends
-         * @param id the id of the trip
-         *
-         */
-        public Trip(String tripName, int locationId, LocalDate tripStartDate, LocalDate tripEndDate, int id) {
+    /**
+     * Instantiates a new User.
+     *
+     * @param tripName      the trip name
+     * @param locationId    the id of the location of the trip
+     * @param tripStartDate the date the trip begins
+     * @param tripEndDate   the date the trip ends
+     * @param id            the id of the trip
+     */
+    public Trip(String tripName, int locationId, LocalDate tripStartDate, LocalDate tripEndDate, int id) {
             this.tripName = tripName;
             this.locationId = locationId;
             this.tripStartDate = tripStartDate;
             this.tripEndDate = tripEndDate;
             this.id = id;
+        }
+
+    /**
+     * Gets location id.
+     *
+     * @return the location id
+     */
+    public int getLocationId() {
+            return locationId;
+        }
+
+    /**
+     * Sets location id.
+     *
+     * @param locationId the location id
+     */
+    public void setLocationId(int locationId) {
+            this.locationId = locationId;
+        }
+
+    /**
+     * Gets trip name.
+     *
+     * @return the trip name
+     */
+    public String getTripName() {
+            return tripName;
+        }
+
+    /**
+     * Sets trip name.
+     *
+     * @param tripName the trip name
+     */
+    public void setTripName(String tripName) {
+            this.tripName = tripName;
+        }
+
+    /**
+     * Gets trip start date.
+     *
+     * @return the trip start date
+     */
+    public LocalDate getTripStartDate() {
+            return tripStartDate;
+        }
+
+    /**
+     * Sets trip start date.
+     *
+     * @param tripStartDate the trip start date
+     */
+    public void setTripStartDate(LocalDate tripStartDate) {
+            this.tripStartDate = tripStartDate;
+        }
+
+    /**
+     * Gets trip end date.
+     *
+     * @return the trip end date
+     */
+    public LocalDate getTripEndDate() {
+            return tripEndDate;
+        }
+
+    /**
+     * Sets trip end date.
+     *
+     * @param tripEndDate the trip end date
+     */
+    public void setTripEndDate(LocalDate tripEndDate) {
+            this.tripEndDate = tripEndDate;
         }
 }
