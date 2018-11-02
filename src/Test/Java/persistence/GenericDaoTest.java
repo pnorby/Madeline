c34623bce4b55f484c8e25c717f2857d9ad9c634
Paul Norby <paulnorby@me.com>
2018-11-02 05:40:02 -0500

    I added a test file.

---
+++ src/Test/Java/persistence/GenericDaoTest.java
@@ -0,0 +1,14 @@
+package persistence;
+
+import entity.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+class GenericDaoTest {
+
+}
